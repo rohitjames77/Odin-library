913be89c97ea9bba6868985c001476d98059b6ae
rohit james <rohitjames77@outlook.com>
2024-04-29 21:01:14 +0530

    Add improvement to createCard func

--- main.js
+++ main.js
@@ -20,28 +20,34 @@ formSubmit.addEventListener('click',addBookToLibrary);
 function addBookToLibrary(){
 let input = document.querySelector('.input');
   if(input.value.length != 0){
-  let title = document.querySelector('.input-title').value;
+let title = document.querySelector('.input-title').value;
 let author = document.querySelector('.input-author').value;
 let pages  = document.querySelector('.input-pages').value;
-console.log("title  " + title,'Author ' + author, 'Pages ' + pages,'read ' + read);
+console.log("title  " + title,'Author ' + author, 'Pages ' + pages);
 let book = new Book(title,author,pages)
 console.log(book);
 library.push(book);
-createCard(library)
 document.querySelector('.formContainer').style.display ='none';
 }
+
 }
 
-function createCard(library){
-  for (let i = 0; i < library.length; i++) {
-    let book = document.createElement('div');   
-    book.className = 'bookCard';
-    document.querySelector('.bookContainer').appendChild(book);  
-   let values = Object.values(library[i]);
-    book.textContent = values;
-    let read
+formSubmit.addEventListener('click',createCard);
+function createCard(){
+  let bookDiv = document.createElement('div');   
+  bookDiv.className = 'bookCard';
+  document.querySelector('.bookContainer').appendChild(bookDiv);  
   }
-}
 
+  formSubmit.addEventListener('click',appendTextValue);
+function  appendTextValue (){
+library.forEach(obj => {
+  let value = Object.values(obj);
+  console.log(` Value ${value}`);
+  let bookDiv = document.querySelector('.bookCard');
+  bookDiv.textContent = value; 
+});
+
+}
 
 
