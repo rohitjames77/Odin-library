32cc5997759f3b12cefe2358abd63815af34a624
rohit james <rohitjames77@outlook.com>
2024-04-29 08:40:31 +0530

    Add createCard function

--- main.js
+++ main.js
@@ -1,39 +1,47 @@
 let library =[];
 
-function Book (title,author,pages,read){
+function Book (title,author,pages){
 this.title = title,
 this.author = author,
-this.pages = pages,
-this.read =read
+this.pages = pages
 };
-
+//Add Book Button Function ....................................
 let btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-  document.querySelector('.container').style.display ='block';
+  document.querySelector('.formContainer').style.display ='block';
 })
+// Form Cancel Button Function..................................................
 let cancel = document.querySelector('.cancel').addEventListener('click',()=> {
-    document.querySelector('.container').style.display ='none';
+  document.querySelector('.formContainer').style.display ='none';
 });
-
+// Form Submit Button Function ..................................................... 
 let formSubmit = document.querySelector('.submit');
 formSubmit.addEventListener('click',addBookToLibrary);
 function addBookToLibrary(){
-let title = document.querySelector('.input-title').value;
+let input = document.querySelector('.input');
+  if(input.value.length != 0){
+  let title = document.querySelector('.input-title').value;
 let author = document.querySelector('.input-author').value;
 let pages  = document.querySelector('.input-pages').value;
-let read = document.querySelector('.input-read').value;
 console.log("title  " + title,'Author ' + author, 'Pages ' + pages,'read ' + read);
-let book = new Book(title,author,pages,read)
+let book = new Book(title,author,pages)
 console.log(book);
 library.push(book);
-console.log(library);
 createCard(library)
+document.querySelector('.formContainer').style.display ='none';
+}
 }
 
 function createCard(library){
   for (let i = 0; i < library.length; i++) {
-    console.log(library[i]);  
+    let book = document.createElement('div');   
+    book.className = 'bookCard';
+    document.querySelector('.bookContainer').appendChild(book);  
+   let values = Object.values(library[i]);
+    book.textContent = values;
+    let read
   }
 }
+
 
 
